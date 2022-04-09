7ef993cf97d3a52de0f5f34a4ea2437d6d5689af
shunsuke kaneko <61620829+shunsukekaneko72106@users.noreply.github.com>
2022-04-09 19:09:25 +0900

    基礎（テンプレート構文）

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
+    <HelloWorld />
   </div>
 </template>
 
@@ -11,8 +10,8 @@ import HelloWorld from "./components/HelloWorld";
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 };
 </script>
 
