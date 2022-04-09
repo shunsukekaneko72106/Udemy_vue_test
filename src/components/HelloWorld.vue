98f387c80297094df06818808d5a69a655b55d4b
shunsuke kaneko <61620829+shunsukekaneko72106@users.noreply.github.com>
2022-04-09 19:31:57 +0900

    v-bindの使い方

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,8 +4,16 @@
     <!-- 二重括弧内はjavascriptの動き -->
     <p>{{ number + 3 }}</p>
 
-    <!-- javascriptの関数が実行できる -->
+    <!-- javascriptの関数が実行できる（単一の式のみ） -->
     <p>{{ sayHi() }}</p>
+
+    <!-- vから始まる属性を"ディレクティブ"と呼ぶ -->
+    <p v-text="messege"></p>
+
+    <!-- 属性の値をデータに対応付ける -->
+    <a :href="url">ここからアクセス</a>
+    !-- 省略もできる -->
+    <a v-bind:href="url">ここからアクセス</a>
   </div>
 </template>
 
@@ -15,11 +23,13 @@ export default {
     return {
       messege: "こんにちわ！",
       number: 3,
+      url: "https://www.yahoo.co.jp/",
     };
   },
   methods: {
     sayHi: function () {
-      return "hi!";
+      //インスタンス内のプロパティにアクセスする場合にthisを使う
+      this.messege = "Hey に変更";
     },
   },
 };
